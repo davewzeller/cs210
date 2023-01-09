169bad643bd3b5d86cde0e99f9d3f3e96863124e
Dave Zeller <59622979+davewzeller@users.noreply.github.com>
2023-01-09 15:45:31 -0800

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        int magicNumber;
+        int intGuess;
+
+        Random randomGenerator = new Random();
+
+
+
+
+        string Number = Console.ReadLine();
+        magicNumber = randomGenerator.Next(1, 100);
+
+        do
+        {
+
+
+            Console.Write("What is your guess? ");
+            string Guess = Console.ReadLine();
+            intGuess = int.Parse(Guess);
+
+            if (intGuess < magicNumber)
+            {
+                Console.Write("Higher\n");
+
+            }
+            else if (intGuess > magicNumber)
+            {
+
+                Console.Write("Lower\n");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
+        while (intGuess != magicNumber);
     }
 }
